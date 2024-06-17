d579d68836fdcf9933cbdc714c610e63a71ee0cd
den1so <denrashodchikov@ya.ru>
2024-06-17 18:04:53 +0300

    HW02 part1
    -implemented InMemoryUserRepository
    -add support user for MealRepository,InMemoryMealRepository
    -add MealRestController

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,46 +1,67 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
+    private final Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.meals.forEach(this::save);
+        repository.put(SecurityUtil.authUserId(), new ConcurrentHashMap<>());
+        MealsUtil.meals.forEach(meal -> save(SecurityUtil.authUserId(), meal));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(int userId, Meal meal) {
+        log.info("save {} for userId {}", meal, userId);
+        Map<Integer, Meal> mealMap = repository.get(userId);
+        if (mealMap == null) {
+            return null;
+        }
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
+            mealMap.put(meal.getId(), meal);
             return meal;
         }
-        // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        return mealMap.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
     }
 
     @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    public boolean delete(int userId, int id) {
+        log.info("delete meal {} for userId {}", id, userId);
+        Map<Integer, Meal> mealMap = repository.get(userId);
+        return mealMap != null && mealMap.remove(id) != null;
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int userId, int id) {
+        log.info("get meal {} for userId {}", id, userId);
+        Map<Integer, Meal> mealMap = repository.get(userId);
+        return mealMap != null ? mealMap.get(id) : null;
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public Collection<Meal> getAll(int userId) {
+        log.info("getAll for user {}", userId);
+        Map<Integer, Meal> mealMap = repository.get(userId);
+        return mealMap == null ? Collections.emptyList() :
+                mealMap.values().stream().sorted(Comparator.comparing(Meal::getDate).reversed()).collect(Collectors.toList());
     }
 }
 
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,53 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+@Controller
 public class MealRestController {
-    private MealService service;
+    protected final Logger log = LoggerFactory.getLogger(MealRestController.class);
 
+    private final MealService service;
+
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
+
+    public Meal create(Meal meal) {
+        log.info("userId {} create {}", SecurityUtil.authUserId(), meal);
+        checkNew(meal);
+        return service.create(SecurityUtil.authUserId(), meal);
+    }
+
+    public Meal update(Meal meal, int mealId) {
+        log.info("userId {} update {}", SecurityUtil.authUserId(), meal);
+        assureIdConsistent(meal, mealId);
+        return service.update(SecurityUtil.authUserId(), meal);
+    }
+
+    public void delete(int mealId) {
+        log.info("userId {} delete {}", SecurityUtil.authUserId(), mealId);
+        service.delete(SecurityUtil.authUserId(), mealId);
+    }
+
+    public Meal get(int mealId) {
+        log.info("userId {} get {}", SecurityUtil.authUserId(), mealId);
+        return service.get(SecurityUtil.authUserId(), mealId);
+    }
+
+    public List<MealTo> getAll() {
+        log.info("getAll");
+        return MealsUtil.getTos(service.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay());
+    }
 }
