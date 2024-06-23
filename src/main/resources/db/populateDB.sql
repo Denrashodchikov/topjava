226714f2a5c3524ae0dad2a3f0fb7001feccdf29
den1so <denrashodchikov@ya.ru>
2024-06-23 20:35:49 +0300

    HW03 part1
    -add table 'meals' to DB
    -implemented JdbcMealRepository

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_role;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -10,3 +11,7 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id,datetime,description,calories)
+VALUES (100000,CURRENT_TIMESTAMP,'Test desc', 1000),
+       (100001,CURRENT_TIMESTAMP - (2 * interval '1 day'),'Test desc 2', 2000);
